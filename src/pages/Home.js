c90a5ae1c8ed09cb4f597bcb30f7be7ad052fdbf
Gisbitus <luigipanetta@pm.me>
2022-09-28 16:52:42 +0200

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated, easings } from 'react-spring';
+import '../style/landing.css';
+
+
+function Landing(){
+  return(
+    <>
+      <h1>test</h1>
+    </>
+  )
+}
+
+export default Landing;
