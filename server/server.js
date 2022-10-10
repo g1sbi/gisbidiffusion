1f8bcb5764a14d999792d1cc8da17a38c0c114c3
Gisbitus <luigipanetta@pm.me>
2022-10-10 22:42:18 +0200

    started confirmation email, switched database to self-hosted one

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const User = require('./user');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Gisbi:EZRoZMecXkAjPmwo@cluster0.jkskzen.mongodb.net/?retryWrites=true&w=majority'),{
+mongoose.connect('mongodb://192.168.3.10:8000/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0'),{
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 },() =>{
@@ -37,11 +37,24 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./passportConfig')(passport);
 
+//confirmation code computation
+
+const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+let token = '';
+for (let i = 0; i < 25; i++){
+	token += characters[Math.floor(Math.random() * characters.length)];
+}
+
 //routes
 app.post('/login', (req,res, next) => {
 	passport.authenticate('local', (err,user,info) =>{
 		if (err) throw err;
 		if (!user) res.send('No user exists');
+		if (user.status != 'Active'){
+			return res.status(401).send({
+				res.send('Pending account. Please verify your email')
+			});
+		}
 		else{
 			req.login((user,err) => {
 				if (err) throw err;
--- server/user.js
+++ server/user.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 const user = new mongoose.Schema({
 	username: String,
-	password: String
+	email: String,
+	password: String,
+	status: {
+		type: String,
+		enum: ['Pending','Active'],
+		default: 'Pending'
+	},
+	confirmationCode: {
+		type: String,
+		unique: true
+	},
+	roles: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Role"
+		}
+	]
 });
 
 module.exports = mongoose.model('User',user);
